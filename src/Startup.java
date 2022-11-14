8cc5534e5921e6025de7f7f98724d9cd34d9350a
hbarton2 <hbarton1337@gmail.com>
2022-11-14 13:33:07 -0800

    Fixed Startup.java to execute cli

--- src/Startup.java
+++ src/Startup.java
@@ -3,5 +3,6 @@ import sbw.project.cli.CommandLineInterface;
 public class Startup {
     public static void main(String[] args) {
         CommandLineInterface cli = new CommandLineInterface();
+        cli.execute();
     }
 }
